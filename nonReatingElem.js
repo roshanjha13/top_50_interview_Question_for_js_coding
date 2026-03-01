816dbc2706d9c0a0650626acf28e61b3653ecadb
roshanjha13 <jharoshan618@gmail.com>
2026-03-01 19:39:06 +0530

    code changed

--- nonReatingElem.js
+++ nonReatingElem.js
@@ -1,18 +1,25 @@
-let arr = [1,1,2,3,2,3,5]
+let arr = [1,1,2,3,2,3,5];
 
-function xyz(arr){
+function nonRepeatingElem(arr){
     let map = new Map();
 
+    // Step 1: Count frequency
     for (let i = 0; i < arr.length; i++) {
         if (map.has(arr[i])) {
-            map.set(arr[i],map.get(arr[i])+1)
+            map.set(arr[i], map.get(arr[i]) + 1);
         } else {
-            map.set(arr[i],1)
+            map.set(arr[i], 1);
         }
-        
     }
 
-        
+    // Step 2: Find first non-repeating
+    for (let i = 0; i < arr.length; i++) {
+        if (map.get(arr[i]) === 1) {
+            return arr[i];
+        }
+    }
+
+    return null; // agar koi non-repeating na mile
 }
 
-console.log(xyz(arr));
+console.log(nonRepeatingElem(arr));
