41e77fde2e4bd51f22ee4d693868574bcb6dd7a1
roshanjha13 <jharoshan618@gmail.com>
2026-03-04 12:22:31 +0530

    Object to array

---
+++ objectToArray.js
@@ -0,0 +1,12 @@
+let obj = {
+  name: "Roshan",
+  age: 25,
+  city: "Pune"
+}
+
+let ans = Object.entries(obj)
+let ans2 = Object.keys(obj)
+let ans3 = Object.values(obj)
+
+console.log(ans,ans2,ans3);
+
